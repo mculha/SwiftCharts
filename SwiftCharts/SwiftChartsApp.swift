8777fbd61ca10a91412c1226d82aa8a0a4b9bddc
mculha <melih.culha.09@gmail.com>
2023-10-04 23:04:11 +0300

    Initial Commit

---
+++ SwiftCharts/SwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsApp.swift
+//  SwiftCharts
+//
+//  Created by Melih Çulha on 4.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
