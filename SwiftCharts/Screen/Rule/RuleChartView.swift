d269b929a63e9e6b6ebba908e9ae71bfb63f4529
mculha <melih.culha.09@gmail.com>
2023-10-09 23:05:20 +0300

    Adding a Rule Mark on the Line Mark to Show Average of the Data

--- SwiftCharts/Screen/Rule/RuleChartView.swift
+++ SwiftCharts/Screen/Rule/RuleChartView.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import Charts
 
 struct RuleChartView: View {
+    
+    private let viewModel: RuleChartViewModel = .init()
+    
     var body: some View {
-        Text("Rule Chart View")
+        VStack {
+            Chart {
+                ForEach(viewModel.permits) { permit in
+                    LineMark(x: .value("Year", permit.date),
+                             y: .value("Number of Permits", permit.numberOfPermits))
+                    .foregroundStyle(by: .value("Region", permit.region))
+                }
+                
+                RuleMark(y: .value("Average", viewModel.averageNumberOfPermits))
+                    .foregroundStyle(.red)
+            }
+            .frame(height: 400)
+            .padding()
+        }
     }
 }
 
